074869f34ae0a2264b3238a2f9e9f429412b2c0f
Taha-Shaban-Kamel <tahashaban473@gmail.com>
2023-03-13 00:09:41 +0200

    last

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
-function myfun(x){
-    x.classlist.toggle("change");
+let menu = document.getElementsByClassName("drobMenu");
+function myfun(x) {
+    x.classList.toggle("change");
+    menu.classList.toggle("drobMenu_visible");
 }
+let sub = document.getElementById("subM");
+let borImg = document.getElementsByClassName("usImg");
+function toggleM(_y) {
+    sub.classList.toggle("user_info_visible");
+}
+function toggleImg(_y) {
+    borImg.classList.toggle("usrImg");
+    
+}
+    
